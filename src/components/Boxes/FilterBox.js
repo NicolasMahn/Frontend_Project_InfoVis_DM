588d3efc233434636a051218c5ada27ac5f4741b
Nina Marie Lauxmann <nl222zc@student.lnu.se>
2025-01-08 08:44:29 +0100

    update Landing page/test button child graph

--- src/components/Boxes/FilterBox.js
+++ src/components/Boxes/FilterBox.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import defaultCookie from './defaultCookie.json'; // Import the defaultCookie.json file
 
-const FilterBox = ({ filters, onFilterChange, filterSettings, config}) => {
-    return (
-        <div className="filter-box">
-            {filters.map((FilterComponent, index) => (
-            <FilterComponent key={index} onFilterChange={onFilterChange} filterSettings={filterSettings} config={config}/>
-            ))}
-        </div>
-    );
+const FilterBox = ({ filters, onFilterChange, filterSettings, config }) => {
+  const handlePreselection = (preselectedFilters) => {
+    // Wenden Sie die Preselection auf die Filter an
+    onFilterChange(preselectedFilters);
+  };
+
+  // Filtern Sie die Preselections, die den aktuellen Graph als Parent haben
+  const preselections = defaultCookie.filter(item => item.parent === config.graph);
+
+  return (
+    <div className="filter-box">
+      {preselections.map(preselection => (
+        <button key={preselection.id} onClick={() => handlePreselection(preselection.filterSettings)}>
+          Load Preselection {preselection.title}
+        </button>
+      ))}
+      {filters.map((FilterComponent, index) => (
+        <FilterComponent key={index} onFilterChange={onFilterChange} filterSettings={filterSettings} config={config} />
+      ))}
+    </div>
+  );
 };
 
 export default FilterBox;
--- src/components/Graphs/LandingPage.js
+++ src/components/Graphs/LandingPage.js
@@ -3,31 +3,25 @@ import React, { useState, useEffect } from 'react';
 const LandingPage = () => {
     return (
         <div>
-            <header>
-                <h2>Welcome to the Gastech Employee Investigation Dashboard</h2>
-            </header>
             <section>
-                <h3>What it's about:</h3>
+                <h4>What it's about:</h4>
                 <p>The project focuses on analyzing GASTech employee data related to vehicle tracking, credit card, and loyalty card transactions. 
                     The goal is to support law enforcement in identifying suspicious patterns in employee behavior leading up to their disappearance. 
                     This investigation utilizes visual analytics to explore the interactions between these data sources, aiming to uncover hidden relationships, anomalies, and suspicious activities. </p>
             </section>
             <section>
-            <h3>The Graphs</h3>
-                <h4>Purchases per Location</h4>
-                <p>This Graph shows how many purchases have been made for each location.</p>
-                <h4>Puchases over Time</h4>
-                <p>This Graph shows how the number of purchases evolves through differnet time categories. 
-                    It is possible to see when a specific location is popular Through the week or through a typical day.</p>
-                <h4>Credit and Loyalty Card Matrix</h4>
-                <p>In this Graph we matched loyalty cards and credit crads to each other based on time and location they were used.</p>
-                <h4>Cars and Credit Card Matrix</h4>
-                <p>In this Graph we tried to match cars and credit cars based on their location and purchases to each other.</p>
-                <h4>Location and Emloyee Map</h4>
-                <p>This Map shows clusters of parking cars (color) and points where employees have met minimum 2 times for 10 minutes in the abila street network.</p>
+            <h4>The Graphs</h4>
+            <ul>
+                <li>Purchases per Location: This Graph shows how many purchases have been made for each location.</li>
+                <li>Puchases over Time: This Graph shows how the number of purchases evolves through differnet time categories. 
+                    It is possible to see when a specific location is popular Through the week or through a typical day.</li>
+                <li>Credit and Loyalty Card Matrix: In this Graph we matched loyalty cards and credit crads to each other based on time and location they were used.</li>
+                <li>Cars and Credit Card Matrix: In this Graph we tried to match cars and credit cars based on their location and purchases to each other.</li>
+                <li>Location and Emloyee Map: This Map shows clusters of parking cars (color) and points where employees have met minimum 2 times for 10 minutes in the abila street network.</li>
+            </ul>
             </section>
             <section>
-                <h3>How to use:</h3>
+                <h4>How to use:</h4>
                 <ul>
                     <li>For each graph we provided a Filter selection as default to introduce important and interseting data.</li>
                     <li>Other interesting filter setting like location categories can be found as buttons in the filter section.</li>
@@ -37,7 +31,7 @@ const LandingPage = () => {
                     </ul> 
             </section> 
             <section>
-                <h3>Suspecios Behavior:</h3>
+                <h4>Suspecios Behavior:</h4>
                 <p>We could detect different suspecious behaviors through our analysis:</p>
                 <ul>
                     <li>Cars: Not all Cars or Trucks are attached to a employee.</li>
